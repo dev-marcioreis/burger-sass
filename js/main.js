a07bd42b5ffc6c66cb0d63636bd312af63de5494
Marcio Reis <dev.marcioreis@gmail.com>
2023-01-21 14:18:19 -0300

    navbar js pronto

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const openBtn = document.getElementById('menu-open-btn')
+const closeBtn = document.getElementById('menu-close-btn')
+const navbar = document.querySelector('.navbar')
+
+openBtn.addEventListener('click', () => {
+    navbar.style.display = 'flex'
+    openBtn.style.display = 'none'
+    closeBtn.style.display = 'inline-block'
+})
+
+const closeNavbar = () => {
+    navbar.style.display = 'none'
+    openBtn.style.display = 'inline-block'
+    closeBtn.style.display = 'none'
+}
+
+closeBtn.addEventListener('click', closeNavbar)
+
+if(window.innerWidth < 1024) {
+    document.querySelectorAll('.navbar a').forEach(navbar => {
+        navbar.addEventListener('click', () => {
+            closeNavbar()
+        })
+    })
+}
